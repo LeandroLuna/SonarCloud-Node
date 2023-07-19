e260d5ff305526610520cd8e17d30be3581cf6b0
Leandro Luna <leandro.j.luna@gmail.com>
2023-07-19 10:21:19 -0300

    test: :white_check_mark: add new test to PR

--- src/math.test.js
+++ src/math.test.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-=======
-// math.test.js
->>>>>>> efc6106 (ci: add github actions and math func + test)
 const doubleNumber = require('./math');
 
 test('Should double the given number', () => {
   expect(doubleNumber(3)).toBe(6);
+  expect(doubleNumber(5)).toBe(10);
 });
